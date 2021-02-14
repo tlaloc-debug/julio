416f3bb8e5156630b635acbdd5b2529f475d0611
tlaloc_debug <pelondinho@hotmail.com>
2021-02-14 14:36:50 -0500

    finish responsive design

--- src/fotos.js
+++ src/fotos.js
@@ -1,20 +1,24 @@
 import React from "react";
 import AliceCarousel from 'react-alice-carousel';
 import "react-alice-carousel/lib/alice-carousel.css";
+import { useMediaQuery } from 'react-responsive';
 
 function Fotos() {
+
+    const matchesfotos = useMediaQuery({query: '(min-width:600px)'});
+
     return (
         <div>
-            <div style={{height: "640px", width: "640px"}}>
+            <div className={matchesfotos ? "boxpc" : "boxcell"}>
                 <AliceCarousel autoPlay autoPlayInterval="3000" infinite="true" disableButtonsControls="true" disableDotsControls="true">
-                    <div className="taller1"></div>
-                    <div className="taller2"></div>
-                    <div className="taller3"></div>
-                    <div className="taller4"></div>
-                    <div className="taller5"></div>
-                    <div className="taller6"></div>
-                    <div className="taller7"></div>
-                    <div className="taller8"></div>                    
+                    <div className={matchesfotos ? "tallerpc1" : "tallercell1"}></div>
+                    <div className={matchesfotos ? "tallerpc2" : "tallercell2"}></div>
+                    <div className={matchesfotos ? "tallerpc3" : "tallercell3"}></div>
+                    <div className={matchesfotos ? "tallerpc4" : "tallercell4"}></div>
+                    <div className={matchesfotos ? "tallerpc5" : "tallercell5"}></div>
+                    <div className={matchesfotos ? "tallerpc6" : "tallercell6"}></div>
+                    <div className={matchesfotos ? "tallerpc7" : "tallercell7"}></div>
+                    <div className={matchesfotos ? "tallerpc8" : "tallercell8"}></div>                    
                 </AliceCarousel>
                 
             </div>
--- src/index.js
+++ src/index.js
@@ -17,12 +17,15 @@ function App() {
     return (
         <div>
             <div className={matches ? "headerpc" : "headercell"}>
-
-                <img src={logo} alt="" className={matches ? "logopc" : "logocell"}/>
+                <div className={matches ? "" : "horizontal"}>
+                    <div>
+                        <img src={logo} alt="" className={matches ? "logopc" : "logocell"}/>
+                    </div>
+                </div>
                 
                 <div className={matches ? "centerpc" : "centercell"}>
-                    <div className={matches ? "oficinapc" : "oficinacell"}>
-                        <div style={{fontWeight: "bold"}}> Av. Siglo XXI #5123 Haciendas de Aguasclaientes</div>    
+                    <div className={"oficina"}>
+                        <div style={{fontWeight: "bold", textAlign: "center"}}> Av. Siglo XXI #5123 Haciendas de Aguasclaientes</div>    
                     </div>
                     <div className={matches ? "horizontal" : "out"}>
                         <div style={{padding: "25px", fontWeight: "bold", color: "white", fontSize: "1.5em"}}> Alineacion y Blanceo</div>    
@@ -33,77 +36,80 @@ function App() {
                     
                 </div>
             </div>
+            
+            <Fotos />
 
-                <div className={"precios"}>
-                    <div className={"horizontal"}>
-                        <div style={{padding: "40px 0px"}}>
-                            <div className={"lista"}>
-                                <div className={"marca"}>Diagnostico Gratis</div>
-                            </div>
-                            <div className={"lista"}>
-                                <div>Alineación y Balanceo </div> 
-                                <div>(rines estándar) </div>  
-                                <div className={"marca"}> $200 </div> 
-                            </div>
-                            <div className={"lista"}>
-                                <div>Alineación y Balanceo</div>  
-                                <div>(rines deportivos... 17 hasta 22 </div>
-                                <div>incluyendo plomo de contacto)</div>  
-                                <div className={"marca"}> $350</div>
-                            </div>
-                            <div className={"lista"}>
-                                <div>Cambio de Refacciones</div>  
-                                <div className={"marca"}> 10% de Descuento </div> 
-                            </div>
-                            <div className={"lista"}>
-                                <div>Cambio de Frenos Delantero y traseros</div> 
-                                <div className={"marca"}> 10% de Descuento </div> 
-                            </div>
+            <div className={matches ? "preciospc" : "precioscell"}>
+                <div className={"horizontal"}>
+                    <div className={matches ? "preciospadpc" : "preciospadcell"}>
+                        <div className={matches ? "listapc" : "listacell"}>
+                            <div className={"marca"}>Diagnostico Gratis</div>
+                        </div>
+                        <div className={matches ? "listapc" : "listacell"}>
+                            <div>Alineación y Balanceo </div> 
+                            <div>(rines estándar) </div>  
+                            <div className={"marca"}> $200 </div> 
+                        </div>
+                        <div className={matches ? "listapc" : "listacell"}>
+                            <div>Alineación y Balanceo</div>  
+                            <div>(rines deportivos... 17 hasta 22 </div>
+                            <div>incluyendo plomo de contacto)</div>  
+                            <div className={"marca"}> $350</div>
+                        </div>
+                        <div className={matches ? "listapc" : "listacell"}>
+                            <div>Cambio de Refacciones</div>  
+                            <div className={"marca"}> 10% de Descuento </div> 
+                        </div>
+                        <div className={matches ? "listapc" : "listacell"}>
+                            <div>Cambio de Frenos Delantero y traseros</div> 
+                            <div className={"marca"}> 10% de Descuento </div> 
                         </div>
                     </div>
                 </div>
-            
-            
-            <Fotos />
+            </div>
             
             <div className={"body"}>
-                <div style={{width: "70%", display: "flex", justifyContent: "center"}}>
+                <div className={matches ? "titlebodypc" : "titlebodycell"}>
                     <h2>Alineacion y Balanceo</h2>
                 </div>
-                <div style={{display: "flex", justifyContent: "space-around"}}>
-                    <div style={{width: "70%", marginBottom: "20px"}}>
-                        <div>Sabemos lo importante que es para ti tener tu auto en perfectas condiciones. Por eso es que la alineación y el balanceo juegan un papel esencial en el funcionamiento de un vehículo y es de suma importancia para el desempeño correcto de cualquier automóvil.</div> <br/>
-                        <div style={{fontWeight: "bold"}}>¿CUÁNDO? </div> <br/>
-                        <div>Se recomienda hacerlo cada 6, o bien, si sientes cualquier vibración al momento de viajar a alta velocidad, desgastes inadecuados en tus llantas o siente que la dirección se va hacia un lado, es hora de llevar el vehículo al taller y pasarlo por alineación y balanceo.</div><br/>
-                        <div style={{fontWeight: "bold"}}>¿POR QUÉ?</div><br/>
-                        <div>Por seguridad o durabilidad de las llantas es importante hacer la alineación en los cuatro neumáticos. ¡No se te olvide! De esta manera tu vehículo será más seguro y el desgaste más parejo en tus llantas.</div>
-                        <div>En cuanto al balanceo, sirve para que las llantas giren sin tener algún tipo de vibración a determinadas velocidades.</div>
+                <div className={matches ? "spacepc" : ""}>
+
+                    <div className={ matches ? "out" : "balanceocell"}></div>
+
+                    <div className={matches ? "bodytextpc" : "bodytextcell"}>
+                        <div className={matches ? "parrafopc" : "parrafocell"}>Sabemos lo importante que es para ti tener tu auto en perfectas condiciones. Por eso es que la alineación y el balanceo juegan un papel esencial en el funcionamiento de un vehículo y es de suma importancia para el desempeño correcto de cualquier automóvil.</div> <br/>
+                        <div className={"preguntas"}>¿CUÁNDO? </div> <br/>
+                        <div className={matches ? "parrafopc" : "parrafocell"}>Se recomienda hacerlo cada 6, o bien, si sientes cualquier vibración al momento de viajar a alta velocidad, desgastes inadecuados en tus llantas o siente que la dirección se va hacia un lado, es hora de llevar el vehículo al taller y pasarlo por alineación y balanceo.</div><br/>
+                        <div className={"preguntas"}>¿POR QUÉ?</div><br/>
+                        <div className={matches ? "parrafopc" : "parrafocell"}>Por seguridad o durabilidad de las llantas es importante hacer la alineación en los cuatro neumáticos. ¡No se te olvide! De esta manera tu vehículo será más seguro y el desgaste más parejo en tus llantas.</div>
+                        <div className={matches ? "parrafopc" : "parrafocell"}>En cuanto al balanceo, sirve para que las llantas giren sin tener algún tipo de vibración a determinadas velocidades.</div><br/>
                     </div>
                 
-                    <div className={"balanceo"}></div>
+                    <div className={ matches ? "balanceopc" : "out"}></div>
                         
                 </div>
             </div>
+            
             <br/>
             <div className={"body"}>
                 <div style={{display: "flex", justifyContent: "right"}}> 
-                    <div style={{width: "70%", display: "flex", justifyContent: "center"}}>
-                        <h2>Cambio de pastillas de freno</h2>
+                    <div className={matches ? "titlebodypcrigth" : "titlebodycell"}>
+                        <h2 style={{textAlign: "center"}}>Cambio de pastillas de freno</h2>
                     </div>
                 </div>
                     
-                <div style={{display: "flex", justifyContent: "space-around"}}>
+                <div className={matches ? "spacepc" : ""}>
                     
-                    <div className={"pastillas"}></div>
+                    <div className={matches ? "pastillaspc" : "pastillascell"}></div>
 
-                    <div style={{width: "70%", display: "flex", justifyContent: "center", marginBottom: "20px"}}>
+                    <div className={matches ? "bodytextpc" : "bodytextcell"}>
                         <div>
-                            <div style={{fontWeight: "bold"}}>¿QUE SON?</div><br/>
-                            <div>Las pastillas de freno son uno de los materiales que más desgaste sufren del coche con el paso del tiempo y es necesario cambiarlas, ya que son de suma importancia para tu seguridad y los de alrededor.</div><br/>
-                            <div style={{fontWeight: "bold"}}>¿CUANDO HAY QUE CAMBIARLAS?</div><br/>
-                            <div>Con base en nuestra experiencia nosotros recomendamos cambiarlas mínimo dos veces al año. Dependiendo del uso que le des a tu automóvil (agresividad al volante).Si se escucha un ruido (agudo) al momento de frenar. O bien, si en tu coche se enciende el testigo de desgaste de pastillas.</div><br/>
-                            <div style={{fontWeight: "bold"}}>¿POR QUE HAY QUE CAMBIARLAS?</div><br/>
-                            <div>Si las pastillas se llegaran a gastar en su totalidad, se podría dañar superficie de los discos y ocasionar un accidente.</div> 
+                            <div className={"preguntas"}>¿QUE SON?</div><br/>
+                            <div className={matches ? "parrafopc" : "parrafocell"}>Las pastillas de freno son uno de los materiales que más desgaste sufren del coche con el paso del tiempo y es necesario cambiarlas, ya que son de suma importancia para tu seguridad y los de alrededor.</div><br/>
+                            <div className={"preguntas"}>¿CUANDO HAY QUE CAMBIARLAS?</div><br/>
+                            <div className={matches ? "parrafopc" : "parrafocell"}>Con base en nuestra experiencia nosotros recomendamos cambiarlas mínimo dos veces al año. Dependiendo del uso que le des a tu automóvil (agresividad al volante).Si se escucha un ruido (agudo) al momento de frenar. O bien, si en tu coche se enciende el testigo de desgaste de pastillas.</div><br/>
+                            <div className={"preguntas"}>¿POR QUE HAY QUE CAMBIARLAS?</div><br/>
+                            <div className={matches ? "parrafopc" : "parrafocell"}>Si las pastillas se llegaran a gastar en su totalidad, se podría dañar superficie de los discos y ocasionar un accidente.</div> <br/>
                         </div>
                     </div>   
                 </div>
@@ -111,9 +117,9 @@ function App() {
                 
             
 
-            <div style={{display: "flex", justifyContent: "space-around", marginTop: "20px"}}>
+            <div className={matches ? "contactopc" : ""}>
                 
-                <div style={{width: "30%"}}> 
+                <div className={matches ? "mapapc" : "mapacell"}> 
                     <div className={"horizontal"}>
                         <div style={{height: "40px", color: "#fb7d0c", fontSize: "2em"}}>Contacto</div> 
                     </div>
@@ -124,16 +130,17 @@ function App() {
                     </div>
                     <div style={{marginTop: "30px", marginLeft: "10%"}}>
                         <div style={{position: "absolute"}}><GeoAltFill size={30} color={"red"}/></div>
-                        
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3702.173573041671!2d-102.25458698489214!3d21.88939088553848!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8429f1e24ea2e8d7%3A0x217657bd36c1fbb4!2sAv.%20Siglo%20XXI%205123%2C%20Haciendas%20de%20Aguascalientes%2C%2020190%20Aguascalientes%2C%20Ags.%2C%20M%C3%A9xico!5e0!3m2!1ses!2sca!4v1613241976522!5m2!1ses!2sca" width="600" height="450" frameBorder="0" style={{border:0, height: "300px", width: "80%", marginLeft: "15%"}} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
+                        <div className={matches ? "mapabodypc" : "mapabodycell"}>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3702.173573041671!2d-102.25458698489214!3d21.88939088553848!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8429f1e24ea2e8d7%3A0x217657bd36c1fbb4!2sAv.%20Siglo%20XXI%205123%2C%20Haciendas%20de%20Aguascalientes%2C%2020190%20Aguascalientes%2C%20Ags.%2C%20M%C3%A9xico!5e0!3m2!1ses!2sca!4v1613241976522!5m2!1ses!2sca" width="600" height="450" frameBorder="0" style={{border:0, height: "300px", width: "100%", marginLeft: "15%"}} allowFullScreen="" aria-hidden="false" tabIndex="0"></iframe>
+                        </div>
                     </div>
                 </div>
                 
-                <div style={{width: "50%"}}>
+                <div className={matches ? "correopc" : "correocell"}>
                     <div className={"horizontal"}>
                         <div style={{height: "40px", color: "#fb7d0c", fontSize: "2em"}}>Correo Electronico</div> 
                     </div>
-                    <div style={{paddingLeft: "200px", paddingTop: "20px"}}><Sendemail /></div>
+                    <div className={matches ? "correobodypc" : "correobodycell"}><Sendemail /></div>
                 </div>
                 
             </div>
